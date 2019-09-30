e67becc9ffd431efbf258ac3790bcad7cf4b1a67
Saurabh Shukla <saurabh@yapapaya.com>
2019-09-30 19:19:52 +0530

    Cleanup formatting and add docs

--- includes/modules/certificate-builder/includes/admin/class-llms-certificate-builder-toolbar.php
+++ includes/modules/certificate-builder/includes/admin/class-llms-certificate-builder-toolbar.php
@@ -1,36 +1,69 @@
 <?php
-class LLMS_Certificate_Builder_Toolbar{
+/**
+ * Certificate Builder Toolbar Button
+ *
+ * @package LifterLMS/Modules/Certificate_Builder
+ *
+ * @since [version]
+ * @version [version]
+ */
 
-	public function __construct(){
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * Main Toolbar Button Class
+ *
+ * @since [version] Introduced
+ */
+
+class LLMS_Certificate_Builder_Toolbar {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->hook();
+	}
+
+	/**
+	 * Add the toolbar hooks
+	 *
+	 * @since [version] Introduced
+	 */
+	private function hook() {
 		add_action( 'admin_bar_menu', array( $this, 'builder_link' ), 90 );
 	}
 
 	/**
+	 * Create a toolbar button
 	 *
+	 * @admin_bar WP_Admin_Bar
+	 *
+	 * @since [version] Introduced
 	 */
 	public function builder_link( $admin_bar ) {
 
 		global $post;
 
-		if ( is_admin() ) {
-			$current_screen = get_current_screen();
-
-			if (! ( 'post' == $current_screen->base
-				&& 'add' != $current_screen->action
-				&& current_user_can( 'read_post', $post->ID )
-			 )) {
-				return;
-			}
-		}
-
-
+		// bail on other screens
 		if ( empty( $post ) ) {
 			return;
 		}
 
-		// Check for your post type.
-		if ( $post->post_type === "llms_certificate" && current_user_can( 'edit_post', $post->ID ) ) {
+		// Check if user has the right permissions to see the button.
+		if ( is_admin() ) {
+			$current_screen = get_current_screen();
+
+			if ( ! ( 'post' == $current_screen->base
+				&& 'add' != $current_screen->action
+				&& current_user_can( 'read_post', $post->ID )
+			 ) ) {
+				return;
+			}
+		}
+
+		// Only load on certificate screens.
+		if ( 'llms_certificate' === $post->post_type && current_user_can( 'edit_post', $post->ID ) ) {
 			$build_link = llms_certificate_build_url( $post );
 			$admin_bar->add_menu(
 				array(
