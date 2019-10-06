cbf476ef10d823ceaf202a872a443245c51bd218
Saurabh Shukla <saurabh@yapapaya.com>
2019-10-06 12:47:38 +0530

    Cleanup bulk migrator

--- includes/modules/certificate-builder/includes/migration/class-llms-certificate-bulk-migrator.php
+++ includes/modules/certificate-builder/includes/migration/class-llms-certificate-bulk-migrator.php
@@ -19,38 +19,39 @@ class LLMS_Certificate_Bulk_Migrator {
 
 	/**
 	 * Constructor.
+	 *
+	 * @since [version] Introduced
 	 */
 	public function __construct() {
-
 		$this->hook();
-
 	}
 
 	/**
-	 * Hook ui and processing
+	 * Hook ui and processing.
 	 *
 	 * @since [version]
 	 */
 	public function hook() {
+
 		add_filter( 'llms_status_tools', array( $this, 'buttons' ) );
 
 		add_action( 'llms_status_tool', array( $this, 'process' ) );
 	}
 
 	/**
-	 * Render Migration Buttons
+	 * Render Migration Buttons.
 	 *
-	 * @tools array Existing LifterLMS Tools
-	 *
+	 * @param $tools    array Existing LifterLMS Tools
 	 * @since [version] Introduced
 	 *
 	 * @return array
 	 */
 	public function buttons( $tools ) {
 
+		// get all legacied versions.
 		$legacies = $this->get_legacies();
 
-		// no legacy metadata found, not legacy
+		// add bulk migration only if legacies are found.
 		if ( ! empty( $legacies ) ) {
 
 			$count = count( $legacies );
@@ -62,9 +63,10 @@ class LLMS_Certificate_Bulk_Migrator {
 			);
 		}
 
+		// get all migrated versions.
 		$migrated = $this->get_migrated();
 
-		// no legacy metadata found, not legacy
+		// add bulk rollback only if migrated versions are found.
 		if ( ! empty( $migrated ) ) {
 
 			$count = count( $migrated );
@@ -80,9 +82,11 @@ class LLMS_Certificate_Bulk_Migrator {
 	}
 
 	/**
-	 * Get legacy certificates
+	 * Get legacy certificates.
 	 *
-	 * @since [version] Introduced
+	 * @since  [version] Introduced
+	 *
+	 * @return object
 	 */
 	public function get_legacies() {
 
@@ -101,9 +105,11 @@ class LLMS_Certificate_Bulk_Migrator {
 	}
 
 	/**
-	 * Get migrated certificates
+	 * Get migrated certificates.
 	 *
-	 * @since [version] Introduced
+	 * @since  [version] Introduced
+	 *
+	 * @return object
 	 */
 	public function get_migrated() {
 
@@ -123,57 +129,104 @@ class LLMS_Certificate_Bulk_Migrator {
 	 * Process migration/rollback commands
 	 *
 	 * @since [version] Introduced
+	 *
+	 * @return array    List of processed certificate post IDs
 	 */
 	public function process( $tool ) {
+
+		// only process for builder related tools.
 		if ( ! in_array( $tool, array( 'certificates-bulk-migrate', 'certificates-bulk-rollback' ) ) ) {
 			return;
 		}
 
+		// get the method name from the tool action.
 		$process = str_replace( 'certificates-bulk-', '', $tool );
 
-		return $this->$process();
+		$msg_fragment = ( 'migrate' === $process ) ? __( 'Migrated', 'lifterlms' ) : __( 'Rolled back', 'lifterlms' );
+
+		// call appropriate method (migrate/rollback).
+		$results = $this->$process();
+
+		// initialise message array.
+		$messages = array();
+
+		foreach ( $results as $result ) {
+
+			// process errors differently
+			if ( is_wp_error( $result ) ) {
+				$messages[] = $result->get_error_message();
+				continue;
+			}
+
+			if ( is_int( $result ) ) {
+				$messages[] = sprintf( __( '%s certificate with ID %d', 'lifterlms' ), $msg_fragment, $result );
+			}
+		}
+
+		$message = implode( '<br>', $messages );
+
+		// display message
+		llms_add_notice( $message );
+
 	}
 
 	/**
-	 * Migrate legacy certificates
+	 * Migrate legacy certificates.
 	 *
 	 * @since [version] Introduced
+	 *
+	 * @return array    List of migrated IDs
 	 */
 	private function migrate() {
 
-		$migrated = $this->get_migrated();
-
-		if ( empty( $migrated ) ) {
-			return;
-		}
-
-		$migrated_ids = array();
-
-		foreach ( $migrated as $migrated_id ) {
-			$migrated_ids[] = LLMS_Certificate_Migrator::migrate( $migrated_id );
-		}
-
-		return $migrated_ids;
-
-	}
-
-	/**
-	 * Rollback migrated certificates
-	 *
-	 * @since [version] Introduced
-	 */
-	private function rollback() {
-
+		// get all legacy certificates.
 		$legacies = $this->get_legacies();
 
+		// bail if none found.
 		if ( empty( $legacies ) ) {
 			return;
 		}
 
+		// initialise array to store IDs.
+		$migrated_ids = array();
+
+		// loop through legacied certificates and migrate each of them.
+		foreach ( $legacies as $legacy_id ) {
+
+			// populate return array.
+			$migrated_ids[] = LLMS_Certificate_Migrator::migrate( $legacy_id );
+		}
+
+		return $migrated_ids;
+
+
+	}
+
+	/**
+	 * Rollback migrated certificates.
+	 *
+	 * @since [version] Introduced
+	 *
+	 * @return array List of rolled back certificates' post IDs
+	 */
+	private function rollback() {
+
+		// get all migrated certificates.
+		$migrated = $this->get_migrated();
+
+		// bail if none found.
+		if ( empty( $migrated ) ) {
+			return;
+		}
+
+		// initialise array to store IDs.
 		$legacy_ids = array();
 
-		foreach ( $legacies as $legacy_id ) {
-			$legacy_ids[] = LLMS_Certificate_Migrator::rollback( $legacy_id );
+		// loop through migrated certificates and roll each of them back.
+		foreach ( $migrated as $migrated_id ) {
+
+			// populate return array.
+			$legacy_ids[] = LLMS_Certificate_Migrator::rollback( $migrated_id );
 		}
 
 		return $legacy_ids;
